6101c6d6c828f5b0d4927404d45e1925a5cf3119
Vuong Duc Hieu <vg.duchieu0602@gmail.com>
2022-08-03 22:03:00 +0700

    53

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,11 +60,31 @@ let handleDeleteUser = async(req, res) => {
     return res.status(200).json(message);
 }
 
+let getAllCode = async(req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type);
+        console.log(data);
+        return res
+            .status(200)
+            .json(data);
+    } catch (error) {
+        console.log('Get all code error: ', error);
+        return res
+            .status(200)
+            .json({
+                errCode: -1,
+                errMessage: 'Error from server'
+            })
+    }
+}
+
+
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUser: handleGetAllUser,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser
+    handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode
 }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -197,10 +197,34 @@ let updateUserData = (data) => {
     })
 }
 
+let getAllCodeService = (typeInput) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if(!typeInput) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameters"
+                })
+            }else {
+                let res = {};
+                let allcode = await db.Allcode.findAll({
+                    where: {type: typeInput}
+                });
+                res.errCode = 0;
+                res.data = allcode;
+                resolve(res);
+            }
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
 module.exports = {
     handleUserLogin: handleUserLogin,
     getAllUser: getAllUser,
     createNewUser: createNewUser,
     deleteUser: deleteUser,
-    updateUserData: updateUserData
+    updateUserData: updateUserData,
+    getAllCodeService: getAllCodeService
 }
